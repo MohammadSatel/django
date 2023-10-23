f1480cb99de1a42f1411de76e3e67a4fe42b2c8e
MohammadSatel <hamuds95@gmail.com>
2023-10-23 12:53:16 +0300

    test

--- base/views.py
+++ base/views.py
@@ -1,17 +1,24 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
+@api_view(['GET','POST'])
 def index(req):
-    return JsonResponse('hello home', safe=False)
+    return JsonResponse('home', safe=False)
+
 
 def about(req):
     return JsonResponse('about page', safe=False)
 
+
 def books(req):
     return JsonResponse('books page', safe=False)
 
+
 def customers(req):
     return JsonResponse('customers page', safe=False)
+
 
 def loans(req):
     return JsonResponse('loans page', safe=False)
